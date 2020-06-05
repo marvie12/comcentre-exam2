1531742532ac3380a488cd3911cdbac207772bb2
marvie12 <58973336+marvie12@users.noreply.github.com>
2020-06-05 09:57:03 +0800

    Add files via upload

--- comcentre-exam/compute.php
+++ comcentre-exam/compute.php
@@ -1,5 +1,5 @@
-<!-- FOR COMMANLINE -->
 <?php
+// FOR COMMANDLINE
 $ones = ['','one','two','three','four', 'five','six','seven','eight','nine','ten', 'eleven','twelve', 'thirteen',
 		 'fourteen','fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 $tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
